348267c81dc3b64bbaa6fcb054b2ca92b2ada2ce
XbZhang <xbzhang@thoughtworks.com>
2016-04-23 18:49:26 +0800

    根据元素在节点树中的位置来设置样式

--- src/main/webapp/javascriptdom/chapter9/behavior.js
+++ src/main/webapp/javascriptdom/chapter9/behavior.js
@@ -2,3 +2,38 @@
  * Created by XbZhang on 16/4/22.
  */
 
+function getNextElement(node) {
+    if (node.nodeType == 1) {
+        return node;
+    }
+    if (node.nextSibling) {
+        return getNextElement(node.nextSibling);
+    }
+    return null;
+}
+
+function styleHeaderSiblings() {
+    if (!document.getElementsByTagName) return false;
+    var headers = document.getElementsByTagName("h1");
+    var elem;
+    for (var i = 0; i < headers.length; i++) {
+        elem = getNextElement(headers[i].nextSibling);
+
+        elem.style.fontWeight = "bold";
+        elem.style.fontSize = "1.2em";
+    }
+}
+
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(styleHeaderSiblings);
