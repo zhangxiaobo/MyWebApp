4a78a810c372fcd9d90aaa7c7cf86ab77579ec3c
XbZhang <xbzhang@thoughtworks.com>
2016-04-23 20:20:29 +0800

    javascrip动态t 修改样式

--- src/main/webapp/javascriptdom/chapter9/behavior.js
+++ src/main/webapp/javascriptdom/chapter9/behavior.js
@@ -12,17 +12,25 @@ function getNextElement(node) {
     return null;
 }
 
-function styleHeaderSiblings() {
+function styleHeaderSiblings(tag, theclass) {
     if (!document.getElementsByTagName) return false;
-    var headers = document.getElementsByTagName("h1");
+    var headers = document.getElementsByTagName(tag);
     var elem;
     for (var i = 0; i < headers.length; i++) {
         elem = getNextElement(headers[i].nextSibling);
 
-        elem.style.fontWeight = "bold";
-        elem.style.fontSize = "1.2em";
+        // elem.style.fontWeight = "bold";
+        // elem.style.fontSize = "1.2em";
+
+        // elem.className = "intro";
+
+        addClass(elem, theclass);
     }
 }
+
+// addLoadEvent(function () {
+//     styleHeaderSiblings("h1", "intro");
+// })
 
 function addLoadEvent(func) {
     var oldonload = window.onload;
@@ -36,4 +44,51 @@ function addLoadEvent(func) {
     }
 }
 
-addLoadEvent(styleHeaderSiblings);
+// addLoadEvent(styleHeaderSiblings);
+
+function stripeTables() {
+    if (!document.getElementsByTagName) return false;
+    var tables = document.getElementsByTagName("table");
+    var odd, rows;
+    for (var i = 0; i < tables.length; i++) {
+        odd = false;
+        rows = tables[i].getElementsByTagName("tr");
+        for (var j = 0; j < rows.length; j++) {
+            if (odd == true) {
+                // rows[j].style.backgroundColor = "#ffc";
+                addClass(rows[j], "odd");
+                odd = false;
+            } else {
+                odd = true;
+            }
+        }
+    }
+}
+
+addLoadEvent(stripeTables);
+
+function highlightRows() {
+    if (!document.getElementsByTagName) return false;
+    var rows = document.getElementsByTagName("tr");
+    for (var i = 0; i < rows.length; i++) {
+        rows[i].onmouseover = function () {
+            this.style.fontWeight = "bold";
+        }
+        rows[i].onmouseout = function () {
+            this.style.fontWeight = "normal";
+        }
+    }
+}
+
+addLoadEvent(highlightRows);
+
+function addClass(element, value) {
+    if (!element.className) {
+        element.className = value;
+    } else {
+        newClassName = element.className;
+        newClassName += " ";
+        newClassName += value;
+        element.className = newClassName;
+    }
+}
